9089680887fed764c72b824e726e842c31a79e01
dvlara@gmail.com <dvlara@gmail.com@565deec4-676c-5e5f-f252-77f3fa870eb4>
2013-06-11 20:31:23 +0000

    All levels now have music

--- src/loading.c
+++ src/loading.c
@@ -31,15 +31,15 @@ void loaddata (int map[][11][16]) {
 
 void load_music(Mix_Music *bsogame, int round) {
 
-	if ((round == 0) || (round == 5) || (round == 10) || (round == 15))
+	if ((round == 0) || (round == 5) || (round == 10) || (round == 15) || (round == 20) || (round == 25) || (round == 30) || (round == 35) || (round == 40) || (round == 45) || (round == 50) || (round == 55))
 		bsogame = Mix_LoadMUS("../music/stage1.ogg");
-	if ((round == 1) || (round == 6) || (round == 11) || (round == 16))
+	if ((round == 1) || (round == 6) || (round == 11) || (round == 16) || (round == 21) || (round == 26) || (round == 31) || (round == 36) || (round == 41) || (round == 46) || (round == 51) || (round == 56))
 		bsogame = Mix_LoadMUS("../music/stage2.ogg");
-	if ((round == 2) || (round == 7) || (round == 12) || (round == 17))
+	if ((round == 2) || (round == 7) || (round == 12) || (round == 17) || (round == 22) || (round == 27) || (round == 32) || (round == 37) || (round == 42) || (round == 47) || (round == 52) || (round == 57))
 		bsogame = Mix_LoadMUS("../music/stage3.ogg");
-	if ((round == 3) || (round == 8) || (round == 13) || (round == 18))
+	if ((round == 3) || (round == 8) || (round == 13) || (round == 18) || (round == 23) || (round == 28) || (round == 33) || (round == 38) || (round == 43) || (round == 48) || (round == 53))
 		bsogame = Mix_LoadMUS("../music/stage4.ogg");
-	if ((round == 4) || (round == 9) || (round == 14) || (round == 19))
+	if ((round == 4) || (round == 9) || (round == 14) || (round == 19) || (round == 24) || (round == 29) || (round == 34) || (round == 39) || (round == 44) || (round == 49) || (round == 54))
 		bsogame = Mix_LoadMUS("../music/stage5.ogg");
 
 	Mix_PlayMusic(bsogame, -1);
